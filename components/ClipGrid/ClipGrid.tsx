73ef3a347579ec230a293b029faf8dfd3ce6faf9
Magnus Ekse <380919+eksepsjon@users.noreply.github.com>
2024-11-03 17:54:03 +0100

    Components

---
+++ components/ClipGrid/ClipGrid.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+import classes from './clipGrid.module.css';
+import { VideoItem } from '@/models';
+import { ClipPreview } from '../ClipPreview/ClipPreview';
+
+export interface ButtonProps {
+  clips: VideoItem[];
+}
+
+/** Primary UI component for user interaction */
+export const ClipGrid = ({ clips }: ButtonProps) => {
+  return (
+    <div className={classes.grid}>
+      {clips.map((clip) => {
+        return <ClipPreview key={clip.id} clip={clip} />;
+      })}
+    </div>
+  );
+};
